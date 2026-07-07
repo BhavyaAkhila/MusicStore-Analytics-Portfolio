bd292a87ee096ee30b9de6d08130db40e7960c2b
Akhila Gannireddy <akhigani5@gmail.com>
2026-07-08 00:04:07 +0530

    .
    Fix typo in SQL comment and improve clarity.

--- queries/02_Intermediate_Queries.sql
+++ queries/02_Intermediate_Queries.sql
@@ -31,7 +31,7 @@ LIMIT 10;
 
 
 /* Q3: Return all the track names that have a song length longer than the average song length. 
-Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first. */
+Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first.. */
 
 SELECT name,miliseconds
 FROM track
